8533495a7b87cf0f0c350a5db2eedfe8a2675dee
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2024-04-12 18:55:31 +0300

    4ku RFP
    Nodes 6266788

--- src/ChessDotCpp/search.cpp
+++ src/ChessDotCpp/search.cpp
@@ -656,20 +656,11 @@ Score Search::AlphaBeta(ThreadState& threadState, Board& board, Ply depth, const
         //&& plyState.SingularMove.Value == 0
     )
     {
-        // STATIC EVALUATION PRUNING
-        if
-        (
-            depth < 6
-            && !isPrincipalVariation
-        )
-        {
-            constexpr std::array<Score, 6> margins = { 64, 64, 128, 256, 512, 768 };
-            const Score margin = margins[depth - improving];
 
-            if (staticScore - margin >= beta)
-            {
-                return beta;
-            }
+        // REVERSE FUTILITY PRUNING
+        if (depth < 8 && staticScore - 71 * (depth - improving) >= beta)
+        {
+            return beta;
         }
 
         // RAZORING
