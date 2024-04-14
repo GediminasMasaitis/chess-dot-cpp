9d681582b89a015f5d9a66c1f533fa2343b6b64e
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2024-04-14 21:03:52 +0300

    Bullet test
    Bench 7123241

--- src/ChessDotCpp/evaluationNnue2.h
+++ src/ChessDotCpp/evaluationNnue2.h
@@ -4,16 +4,19 @@
 #include "board.h"
 #include "evaluationNnueBase.h"
 
+#include <algorithm>
+
 class EvaluationNnue2 : public EvaluationNnueBase
 {
 public:
-    static constexpr FinalValue scale = 16 * 256;
+    static constexpr FinalValue scale = 128;
 
     static Score Evaluate(const BoardBase& board)
     {
         assert(!board.accumulatorStack.empty());
         auto& accumulators = board.accumulatorStack[board.accumulatorStack.size() - 1].accumulators;
-        FinalValue outputValue = OutputBias;
+
+        FinalValue outputValue = 0;
         for(auto relativeSide = 0; relativeSide < 2; relativeSide++)
         {
             const Color accumulatorColor = relativeSide == 0 ? board.ColorToMove : board.ColorToMove ^ 1;
@@ -21,13 +24,18 @@ public:
             for (auto hiddenIndex = 0; hiddenIndex < HiddenCount; hiddenIndex++)
             {
                 NnueValue hiddenValue = accumulator[hiddenIndex];
-                hiddenValue = std::max(static_cast<NnueValue>(0), hiddenValue);
+                //hiddenValue = std::max(static_cast<NnueValue>(0), hiddenValue);
+                hiddenValue = std::clamp(hiddenValue, static_cast<NnueValue>(0), static_cast<NnueValue>(128));
 
                 const NnueValue hiddenWeight = HiddenWeightses[relativeSide][hiddenIndex];
-                const FinalValue outputAddition = hiddenValue * hiddenWeight;
+                const FinalValue outputAddition = (hiddenValue * hiddenValue) * hiddenWeight;
                 outputValue += outputAddition;
             }
         }
+
+        outputValue /= 128;
+        outputValue += OutputBias;
+
         const Score score = static_cast<Score>(outputValue / scale);
         return score;
     }
