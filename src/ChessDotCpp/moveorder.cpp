3f0730a691c0a2e169c32f4c0a7073f77d41e620
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2024-05-03 22:09:22 +0300

    MVVLVA tweak
    Bench 8449196

--- src/ChessDotCpp/moveorder.cpp
+++ src/ChessDotCpp/moveorder.cpp
@@ -9,7 +9,7 @@ public:
 
     constexpr MvvLvaClass()
     {
-        constexpr EachPiece<MoveScore> pieceScores{ 0, 0, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6 };
+        constexpr EachPiece<MoveScore> pieceScores{ 0, 0, 0, 0, 1, 1, 2, 2, 2, 2, 3, 3, 4, 4, 5, 5 };
 
         for (size_t i = 0; i < Pieces::Count; i++)
         {
